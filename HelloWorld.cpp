2e32793df5d8cbd68cca1ab8757442888e3a3f06
dangshuai <dangshuai@MacBook.local>
2017-05-06 15:23:44 +0800

    cpp hello world

---
+++ HelloWorld.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+  using namespace std;
+  cout << "Hello World!" << endl;
+  return 0;
+}
+
+
+// 1. 在terminal 上 cd 到该文件目录
+// 2. g++ HelloWorld.cpp
+// 3. ./a.out
