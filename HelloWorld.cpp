b40d43ae908bceb705ab0134b82fd28e7cecaa43
dangshuai <dangshuai@MacBook.local>
2017-12-31 17:38:00 +0800

    部分修改

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -3,6 +3,18 @@
 int main() {
   using namespace std;
   cout << "Hello World!" << endl;
+
+  int original = 30;
+  cout << "original = " << original << endl;
+  cout << "original is at address: " << hex << &original << endl;
+
+  int& ref = original;
+  cout << "ref is at address: " << hex << &ref << endl;
+
+  int& ref2 = ref;
+  cout << "ref2 is at address: "<< hex << &ref2 << endl;
+  cout << "ref2 gets alue, ref2 = " << dec << ref2 << endl;
+
   return 0;
 }
 
